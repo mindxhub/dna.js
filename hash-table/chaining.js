9d87d3fb975b2d72dce8fbd30e1593355b394aea
nvdai2401 <nvdai2401@gmail.com>
2020-06-01 13:26:39 +0700

    refactored code in chaining.js following comments

--- hash-table/chaining.js
+++ hash-table/chaining.js
@@ -1,9 +1,10 @@
 const { Node, DoublyLinkedList } = require('../linked-list/doubly-linked-list')
 
+const DEFAULT_CAPACITY = 8
 class ChainingHashTable {
-  constructor(capacity = 8) {
-    this.arr = new Array(capacity)
-    this.capacity = capacity
+  constructor() {
+    this.arr = new Array(DEFAULT_CAPACITY)
+    this.capacity = DEFAULT_CAPACITY
     this.size = 0
   }
 
@@ -14,24 +15,24 @@ class ChainingHashTable {
       linkList.pushFront(value, key)
       this.arr[index] = linkList
       this.size++
-    } else {
-      const isExisting = this.search(key)
-      if (isExisting) {
-        let cur = this.arr[index].tail
-        while (cur) {
-          if (cur.key === key) {
-            cur.value = value
-            break
-          }
-          cur = cur.prev
-        }
-      } else {
-        this.arr[index].pushBack(value, key)
+      if (this.size === this.capacity) {
+        this.grow()
       }
+      return
     }
 
-    if (this.size === this.capacity) {
-      this.extendCapacity()
+    const isExisting = this.search(key)
+    if (!isExisting) {
+      this.arr[index].pushBack(value, key)
+      return
+    }
+    let cur = this.arr[index].tail
+    while (cur) {
+      if (cur.key === key) {
+        cur.value = value
+        break
+      }
+      cur = cur.prev
     }
   }
 
@@ -39,25 +40,26 @@ class ChainingHashTable {
     const index = this.hash(key)
     
     if (!this.arr[index]) return
-    if (this.arr[index] && this.arr[index].length > 1) {
-      let cur = this.arr[index].tail
-      let count = 0
-      while (cur) {
-        if (cur.key === key) {
-          let len = this.arr[index].length
-          this.arr[index].removeAt(len - count - 1)
-          break
-        }
-        count++
-        cur = cur.prev
-      }
-    } else {
+    if (this.arr[index].length === 1) {
       this.arr[index] = undefined
       this.size--
+      if (this.capacity > DEFAULT_CAPACITY && this.size <= Math.floor(this.capacity / 4)) {
+        this.shrink()
+      }
+      return
     }
-    if (this.capacity > 8 && this.size <= Math.floor(this.capacity / 4)) {
-      this.shrinkCapacity()
-    }
+    
+    let cur = this.arr[index].tail
+    let count = 0
+    while (cur) {
+      if (cur.key === key) {
+        let length = this.arr[index].length
+        this.arr[index].removeAt(length - count - 1)
+        break
+      }
+      count++
+      cur = cur.prev
+    } 
   }
 
   search(key) {
@@ -66,16 +68,15 @@ class ChainingHashTable {
     
     if (!this.arr[index]) {
       return null
-    } else {
-      let cur = this.arr[index].tail
-      while (cur) {
-        if (cur.key === key) {
-          return cur.value
-        }
-        cur = cur.prev
-      }
     }
-    return null
+
+    let cur = this.arr[index].tail
+    while (cur) {
+      if (cur.key === key) {
+        return cur.value
+      }
+      cur = cur.prev
+    }
   }
 
   // private methods
@@ -83,14 +84,24 @@ class ChainingHashTable {
     return key % this.capacity
   }
 
-  extendCapacity() {
+  grow() {
     this.capacity *= 2
-    const { newArr, length } = this.clone(this.arr, this.size)
+    let { newArr, length } = this.clone(this.arr, this.size)
+    if (length === this.capacity) {
+      do {
+        this.capacity *= 2
+        const res = this.clone(this.arr, this.size)
+        length = res.length
+        if (length < this.capacity) {
+          newArr = res.newArr
+        }
+      } while (length < this.capacity)
+    }
     this.arr = newArr
     this.size = length
   }
 
-  shrinkCapacity() {
+  shrink() {
     const oldCapacity = this.capacity
     this.capacity /= 2
     const { newArr, length } = this.clone(this.arr, oldCapacity)
@@ -104,29 +115,31 @@ class ChainingHashTable {
 
     for (let i = 0; i < size; i++) {
       if (!arr[i]) continue
-      if (arr[i].length > 1) {
-        let cur = arr[i].head
-        while (cur) {
-          const index = this.hash(cur.key)
-          if (newArr[index]) {
-            newArr[index].pushBack(cur.value, cur.key)
-          } else {
-            const linkList = new DoublyLinkedList()
-            linkList.pushBack(cur.value, cur.key)
-            newArr[index] = linkList
-            length++
-          }
-          cur = cur.next
-        }
-      } else {
+      if (arr[i].length === 1) {
         const linkList = new DoublyLinkedList()
         const item = arr[i].head
         const index = this.hash(item.key)
         linkList.pushBack(item.value, item.key)
         newArr[index] = linkList
         length++
+      } else {
+        let cur = arr[i].head
+        while (cur) {
+          if (length === this.capacity) return { newArr, length }
+          const index = this.hash(cur.key)
+          if (newArr[index]) {
+            newArr[index].pushBack(cur.value, cur.key)
+            continue
+          }
+          const linkList = new DoublyLinkedList()
+          linkList.pushBack(cur.value, cur.key)
+          newArr[index] = linkList
+          length++
+          cur = cur.next
+        }
       } 
     }
+
     return { newArr, length }
   }
 }
